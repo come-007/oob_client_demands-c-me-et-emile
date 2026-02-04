4c7be1efb1cc832e09586a904effddfb080b4568
come-007 <come.piernaz@efrei.net>
2026-02-04 14:03:26 +0100

    Mise à jour des modifications

--- clients.py
+++ clients.py
@@ -4,31 +4,29 @@
 # This script manages client data in a text file.
 # Students: Notice how the code is grouped into logical sections.
 # Each section could eventually become a function!
-import os
-
 FILENAME = "clients.txt"
-
-def setup_db():
-    if not os.path.exists(FILENAME):
-        with open(FILENAME, "w") as f:
-            f.write("id,name,email\n")
 
 def ajouter_client():
     name = input("- Name: ")
     email = input("- Email: ")
-    with open(FILENAME, "r") as f:
-        lignes = f.readlines()
+    
+    f = open(FILENAME, "r")
+    lignes = f.readlines()
+    f.close()
+    
     new_id = len(lignes)
-    with open(FILENAME, "a") as f:
-        f.write(f"{new_id},{name},{email}\n")
-    print(f"client enregistred with the ID {new_id}")
+    
+    f = open(FILENAME, "a")
+    f.write(f"{new_id},{name},{email}\n")
+    f.close()
+    print(f"Client enregistré avec l'ID {new_id}")
 
 def afficher_liste():
     print("\n--- CLIENTS LIST ---")
-    with open(FILENAME, "r") as f:
-        data = f.readlines()[1:] 
+    f = open(FILENAME, "r")
+    data = f.readlines()[1:] 
+    f.close()
     
-
     clients = [line.strip().split(",") for line in data]
     
     for c in clients:
@@ -36,9 +34,8 @@ def afficher_liste():
             print(f"ID: {c[0]} | Name: {c[1]} | Email: {c[2]}")
 
 def menu():
-    setup_db()
     while True:
-        print("\n" + "="*25 + "\n1. Add a New clients\n2. View all clients\n3. Exit Program\n" + "="*25)
+        print("\n1. Add a New client\n2. View all clients\n3. Exit Program")
         choix = input("Action : ")
         if choix == "1":
             ajouter_client()
@@ -50,5 +47,4 @@ def menu():
         else:
             print("Invalid Choice")
 
-if __name__ == "__main__":
-    menu()  
+menu()
